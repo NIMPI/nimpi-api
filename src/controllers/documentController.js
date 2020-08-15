c9c3621272e058d4b56b657f00fd5541e88e1e6d
Jhoni Figueredo <jhonifigueredo@msn.com>
2020-08-15 18:10:45 -0400

    Incluindo validações

--- src/controllers/documentController.js
+++ src/controllers/documentController.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 const multerConfig = require('../config/multer');
 const Document = require('../models/Document');
 
-// Uploado de arquivos
+// Upload de arquivos
 exports.uploadFile = async (req, res) => {
   try {
     // Define nome de algumas variáveis
@@ -28,15 +28,33 @@ exports.uploadFile = async (req, res) => {
 
 // Busca por termo
 exports.findByTerm = async (req, res, next) => {
+  
+  function diacriticSensitiveRegex(string = '') {
+    return string.replace(/a/g, '[a,á,à,ã,â,ä]')
+      .replace(/e/g, '[e,é,ë,è,ê,ẽ]')
+      .replace(/i/g, '[i,í,ï,ì,ĩ,î]')
+      .replace(/o/g, '[o,ó,ö,ò,õ,ô]')
+      .replace(/u/g, '[u,ü,ú,ù,ũ,û]')
+      .replace(/c/g, '[c,ç]');
+  }
+  
   try {
     const urlParameter = req.query.term;
 
-    const document = await Document.find({ title: { $regex: urlParameter } })
-    
-    return res.json(document);
+  //  const document = await Document.find({ title: { $regex: urlParameter, $options: 'i' } })
+  //  const document = await Document.find({ title: { $regex: diacriticSensitiveRegex(urlParameter), $options: 'i' } })
+    const document = await Document.find({ tags: { $regex: diacriticSensitiveRegex(urlParameter), $options: 'i' } })
+  
+
+    if (Object.keys(document).length === 0)
+      return res.status(404).send({ error: 'No documents were found with that term' });
+    else
+      return res.json(document);
+
   } catch (err) {
     return res.status(400).send({ error: 'Error searching for document' });
   }
 }
 
 // Alterar documentos
+
