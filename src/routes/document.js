147c6577d0a4e56fbedcd4021ca72ffee96eb959
Jhoni Figueredo <jhonifigueredo@msn.com>
2020-09-01 21:29:37 -0400

    Habilitando listar todos os documentos

--- src/routes/document.js
+++ src/routes/document.js
@@ -12,6 +12,9 @@ router.post('/document', authController.verification, upload.single('path'), doc
 // Busca por termo
 router.get('/document/findbyterm', documentController.findByTerm);
 
+// Busca todos
+router.get('/document', documentController.findAll);
+
 // Update de arquivos
 router.put('/document/:id', authController.verification, documentController.updateFile);
 
